d6f5f78254d1659850ee55f0f375c8ab1b34306f
grayoj <Mgeraldoj07@gmail.com>
2023-05-29 15:10:20 +0100

    Favicon: Use heuvera public image.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+        <link rel='icon' type='image/x-icon' href='/heuvera.png' />
       </head>
       <body className={manrope.className}>{children}</body>
     </html>
