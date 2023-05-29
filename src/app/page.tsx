9f93bbd462fe8c93c148adda97d146579982685c
grayoj <Mgeraldoj07@gmail.com>
2023-05-29 15:04:55 +0100

    Feat: Implement loading functionality on Page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import useLoadingDelay from './hooks/useLoadingDelay';
+import Preloader from './ui/Preloader';
+
 export default function Home(): JSX.Element {
-  return <></>;
+  const Loading = useLoadingDelay(2000);
+  return <>{Loading ? <Preloader /> : null}</>;
 }
