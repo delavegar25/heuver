263e79e5f4b9093cf5584ca92c3e1db9448ae7fc
grayoj <Mgeraldoj07@gmail.com>
2023-05-29 16:10:14 +0100

    Feat: Include file index.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import useLoadingDelay from './hooks/useLoadingDelay';
+import Index from './modules/Index';
 import Preloader from './ui/Preloader';
 
 export default function Home(): JSX.Element {
   const Loading = useLoadingDelay(2000);
-  return <>{Loading ? <Preloader /> : null}</>;
+  return <>{Loading ? <Preloader /> : <Index />}</>;
 }
