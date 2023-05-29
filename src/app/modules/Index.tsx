6220f7e305bcf8d944975f0c87e78540c1fa36a7
grayoj <Mgeraldoj07@gmail.com>
2023-05-29 20:19:13 +0100

    Feat: Update index page for hero.

--- src/app/modules/Index.tsx
+++ src/app/modules/Index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Navigation from '../components/navigation/Navbar';
+import Hero from '../components/hero/Hero';
 
 const Index: React.FC = () => {
-  return <Navigation />;
+  return (
+    <>
+      <Navigation />
+      <Hero />
+    </>
+  );
 };
 
 export default Index;
