240db4bd90cb8698e34f46611515365e1b0558bc
grayoj <Mgeraldoj07@gmail.com>
2023-05-30 22:36:18 +0100

    Feat: Include MFS Component.

--- src/app/modules/Index.tsx
+++ src/app/modules/Index.tsx
@@ -3,6 +3,7 @@ import Navigation from '../components/navigation/Navbar';
 import Hero from '../components/hero/Hero';
 import Feature from '../ui/Layouts/Features';
 import FeatureCtaSection from '../ui/Layouts/FeatureCTASection';
+import MoreFeatureSection from '../ui/Layouts/MoreFeatureSection';
 
 const Index: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const Index: React.FC = () => {
       <Hero />
       <Feature />
       <FeatureCtaSection />
+      <MoreFeatureSection />
     </>
   );
 };
