8758cc28c20433d896992be9bbaaaf27573108ed
grayoj <Mgeraldoj07@gmail.com>
2023-05-30 13:18:10 +0100

    Feat: include animated pointing arrow to hero.

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import Button from '@/app/ui/Button';
-import { ArrowRightIcon } from '@heroicons/react/solid';
+import { ArrowCircleDownIcon, ArrowRightIcon } from '@heroicons/react/solid';
 import { motion } from 'framer-motion';
 import useAnimatedText from '@/app/hooks/useAnimatedText';
 import { texts } from '@/app/utils/HeroTexts';
 import { transition, variants } from '@/app/utils/HeroMotions';
 import HeroMotion from '@/app/ui/Motions/HeroMotion';
+import { ChevronDownIcon } from '@heroicons/react/outline';
 
 const Hero: React.FC = () => {
   const currentText = useAnimatedText(texts, 4000);
+
   return (
-    <div className='flex items-center justify-center h-screen '>
+    <div className='flex items-center justify-center h-screen'>
       <main className='mx-auto px-4 sm:mt-24 bg-hero mb-52 overflow-hidden'>
         <div className='absolute inset-0'>
           <div className='absolute inset-0 bg-gradient-to-r from-purple via-lavender to-black opacity-20 animate-gradient'></div>
         </div>
         <HeroMotion />
         <div className='text-center'>
-          <h1 className=' text-4xl tracking-tight font-extrabold dark:text-white sm:text-5xl md:text-6xl'>
+          <h1 className='text-4xl tracking-tight font-extrabold dark:text-white sm:text-5xl md:text-6xl'>
             <span className='block xl:inline'>Discover Heuvera</span>{' '}
             <span className='block text-purple dark:text-lighterPurple font-design'>
               <motion.div
@@ -32,7 +34,7 @@ const Hero: React.FC = () => {
             </span>
           </h1>
           <p className='mt-3 max-w-md mx-auto text-base dark:text-white sm:text-lg md:mt-5 md:text-xl md:max-w-3xl'>
-            Heuvera aggregrates the best hotels, apartments and properties that
+            Heuvera aggregates the best hotels, apartments, and properties that
             you would love all into a single experience.
           </p>
           <div className='mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8'>
@@ -42,6 +44,24 @@ const Hero: React.FC = () => {
                 <ArrowRightIcon className='ml-2 -mr-1 w-5 h-5' />
               </Button>
             </div>
+          </div>
+          <div className='mt-8'>
+            <motion.div
+              initial={{ y: 0 }}
+              animate={{ y: [0, 10, 0] }}
+              transition={{
+                duration: 1,
+                repeat: Infinity,
+                repeatType: 'loop',
+                ease: 'easeInOut',
+              }}
+              className='inline-block animate-bounce'
+            >
+              <ArrowCircleDownIcon className='h-6 w-6 text-purple dark:text-lighterPurple' />
+            </motion.div>
+            <p className='dark:text-white'>
+              Press "Y" on your keyboard to Explore
+            </p>
           </div>
         </div>
       </main>
