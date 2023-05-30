1ef4118de3419d50b352568bcb2020700ab6d817
grayoj <Mgeraldoj07@gmail.com>
2023-05-30 13:19:48 +0100

    Feat: Include Feature and CTA Sections

--- src/app/modules/Index.tsx
+++ src/app/modules/Index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navigation from '../components/navigation/Navbar';
 import Hero from '../components/hero/Hero';
+import Feature from '../ui/Layouts/Features';
+import FeatureCtaSection from '../ui/Layouts/FeatureCTASection';
 
 const Index: React.FC = () => {
   return (
     <>
       <Navigation />
       <Hero />
+      <Feature />
+      <FeatureCtaSection />
     </>
   );
 };
